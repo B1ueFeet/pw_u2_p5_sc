17dbdec62db47c8c851a754c3d8dbdad0ff0077c
BlueFeet <b1uefeet.dev@gmail.com>
2024-01-24 17:56:53 -0500

    TAREA 07

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,10 +1,15 @@
 <template>
-<h1 v-if="!winner">Consultando la PokéDex.....</h1>
-<div v-else>
-  <h1>¿Quíen es este Pokémon?</h1>
-  <PokemonImage :pokeId="winner.id" :show="showWinner" />
-  <PokemonOptions @slected="comprobarClick($event)" :pokemons="pokeArr" />
-</div>
+  <h1 v-if="!winner">Consultando la PokéDex.....</h1>
+  <div v-else class="game">
+    <h1>¿Quíen es este Pokémon?</h1>
+    <PokemonImage :pokeId="winner.id" :show="showWinner" />
+    <PokemonOptions @slected="comprobarClick($event)" :pokemons="pokeArr" />
+    <div v-if="showWinner" class="message">
+      <label v-if="!showResult" class="fail"> Fallaste!!</label>
+      <label v-if="showResult" class="right"> Ganaste!!</label>
+      <button @click="reiniciar()">Volver a intentarlo</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,66 +21,81 @@ import getPokeIdFacade from "../helpers/PokemonHelper";
 export default {
   components: { PokemonImage, PokemonOptions },
 
-//esta es una de las fases del ciclo de vidad de un componente
-//BEFORE CREATE SE EJECUTA ANTES DE QUE SE CREE EL COMPONENTE
-  beforeCreate(){
-    console.log("Antes de que se cree el componente")
+  //esta es una de las fases del ciclo de vidad de un componente
+  //BEFORE CREATE SE EJECUTA ANTES DE QUE SE CREE EL COMPONENTE
+  beforeCreate() {
+    console.log("Antes de que se cree el componente");
   },
   //CREATED SE EJECUTA CUANDO SE CREA EL COMPONENTE PERO NO ES MONTADO TODAVIA
-  created(){
-    console.log("Se ha Creado el componente")
+  created() {
+    console.log("Se ha Creado el componente");
   },
   //BEFORE MOUNT SE EJECUTA UNOS INSTANTES ANTES DE QUE SE MONTE EL COMPONENTE
-  beforeMount(){  
-    console.log("el componente esta listo para ser montado")
+  beforeMount() {
+    console.log("el componente esta listo para ser montado");
   },
   //MOUNTED SE EJECUTA CADA VEZ QUE ES MNONTADO EL COMPONENTE
   mounted() {
     console.log("Se monto el componente ");
-    this.cargaInicial()
+    this.cargaInicial();
   },
-  //BEFORE UPDATE SIRVE PARA INTERCEPTAR EL CICLO DE VIDA DE UN COMPONENTE 
+  //BEFORE UPDATE SIRVE PARA INTERCEPTAR EL CICLO DE VIDA DE UN COMPONENTE
   // SE EJECUTA ANTES DE QUEW SE ACTUALICE EL COMPONENTE
-  beforeUpdate(){
+  beforeUpdate() {
     console.log("Antes de que se actualice el componete");
   },
   //UPDATE SE EJECUTA CUANDO SE HA ACTUALIZADO EL COMPONENTE
-  updated(){
-      console.log("Se ha actualizado el componete");
+  updated() {
+    console.log("Se ha actualizado el componete");
   },
   //BEFORE DESTROY YA SE ENCUENTRA DEPRECADO
   //SE EJECUTA ANTES DE DESTRUIR EL COMPONENTE
-  beforeDestroy(){
+  beforeDestroy() {
     console.log("Antes de destruir el componete");
   },
-    // DESTROY YA SE ENCUENTRA DEPRECADO
+  // DESTROY YA SE ENCUENTRA DEPRECADO
   //SE EJECUTA CUANDO SE DESTRUYA EL COMPONENTE
-  beforeDestroy(){
+  beforeDestroy() {
     console.log("destruir el componete");
   },
-  data(){
-    return{
+  data() {
+    return {
       pokeArr: [],
       winner: null,
-      showWinner : false,
-    }
+      showWinner: false,
+      showResult: false,
+    };
   },
   methods: {
     async cargaInicial() {
-     var pokemons = await getPokeIdFacade();
+      var pokemons = await getPokeIdFacade();
       console.log("desde componente");
       console.log(pokemons);
-      this.pokeArr =pokemons;
-      this.winner = this.pokeArr[Math.floor(Math.random()*4)]
+      this.pokeArr = pokemons;
+      this.winner = this.pokeArr[Math.floor(Math.random() * 4)];
     },
-    comprobarClick(e){
-      console.log("se selecciono el:" + e)
-      this.showWinner = true
-      if (this.winner.id === e) console.log("GANADOR")
-    }
+    comprobarClick(e) {
+      console.log("se selecciono el:" + e);
+      this.showWinner = true;
+      if (this.winner.id === e) this.showResult = true;
+    },
+    reiniciar() {
+      this.pokeArr = [];
+      this.winner = null;
+      this.showWinner = false;
+      this.showResult = false;
+      this.cargaInicial();
+    },
   },
 };
 </script>
 
 <style>
+.message{
+  display: grid;
+justify-content: center;
+}
+
+
+
 </style>
